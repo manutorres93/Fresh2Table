7b8f540a97d38797b65ead34b80de11b519a5860
Juanja1712 <jara.171201@gmail.com>
2024-01-22 06:47:50 -0500

    J - Agregué buscador input

--- js/function.js
+++ js/function.js
@@ -5,32 +5,33 @@ document.addEventListener('DOMContentLoaded', ()=>{ /* Mientras se cargue el DOM
 
 });
 
-/*---------------SELECTORES---------------*/
+/*---------------VARIABLES Y SELECTORES---------------*/
 
-const inputSearch = document.getElementById('input-search')
+// STEP 1 (Buscador) - Señalar el inputSearch
+const inputSearch = document.getElementById('input-search');
+// STEP 2 (Buscador) - Crear una variable para guardar lo que la persona escribió
+const criterioSeleccionado = { // Se crea para guardar el valor que se esté buscando en ese momento 
+    name : '' // Aqui se guarda el critero seleccionado
 
-
-/*---------------BUSCADOR---------------*/
-
+};
+const iconSelectorFruits = document.querySelector('.icon-selector-fruits');
 
 
 /*---------------INYECTAR LAS CARDS--------------- */
 function showProducts(products) {
-  
+    
     /* Selector para tarjetas */
     const cardContainer = document.querySelector('#cardContainer');
-
-    
     
     products.forEach((product)=>{
         const productCard = document.createElement('div');
-        
+         
         
         productCard.innerHTML = `
         <div class="separador">
         <div class="image">
         <figure>
-        <img src="/Img/${product.image}" alt="">
+        <img src="../Img/${product.image}" alt="">
         </figure>
         </div>
         
@@ -57,27 +58,78 @@ function showProducts(products) {
         `;
         cardContainer.appendChild(productCard);
     });
-
-        
-    inputSearch.addEventListener('keyup', event =>{
-        const prueba = document.getElementById('prueba')
-
-        prueba.innerHTML = `
-        <p>${event.target.value}</p>
-        `
-        
-        //console.log(products.name);
-
-        // if (event.target.matches('.productName')) {
-            
-        //     document.querySelectorAll(products.forEach( fruit =>{
-
-        //         fruit.textContent.includes(event.target.value)
-        //             ?fruit.classList.remove('filtro')
-        //                 :fruit.classList.add('filtro')
-        //     }));
-        // };
     
-    });
 };
 
+/*---------------BUSCADOR---------------*/
+
+// STEP 3 (Buscador) - Crearle el addEventListener
+inputSearch.addEventListener('input', event =>{
+    //console.log(event.target.value);
+    
+    // STEP 4 (Buscador) - Igualar lo que la persona escribió, a nuestra variable donde ibamos a guardar el valor
+    criterioSeleccionado.name = event.target.value
+    // console.log(criterioSeleccionado.name);
+    
+    //  STEP 5 (Buscador) - Llamar a la funcion de filtrar
+    filterName(); 
+});
+
+// STEP 6 (Buscador) 
+function filterName() {
+    const product = products.filter(filterByName); // Filtra segun lo que le pasemos en los parametros
+
+    console.log(product); // Se convierte en un array nuevo con los parametros filtrados
+    console.log(product[0]);
+    console.log(product[0].name);
+    
+    // STEP 10 (Buscador)  - Llamar a la funcion de limpiar 
+    cleanCards(); // La función se llama antes porque debemos primero limpiar y despues mostrar
+
+    // STTEP 8 (Buscador) - Llamar a la funcion que imprime las cards
+    showProducts(product); // 'product' porque estamos mostrando el producto ya filtrado
+
+};
+
+// STEP 7 (Buscador)
+function filterByName(products) { // El parametro es el nombre de la lista en la que vamos a buscar
+    if (criterioSeleccionado.name) { // Si 'criterioSeleccionado.name' existe, entonces... 
+        return products.name == criterioSeleccionado.name.toLowerCase();
+    } else {
+        return products
+    };
+};
+
+// STEP 9 (Buscador) - Funcion para eliminar las otras cards
+function cleanCards() {
+    const cardContainer = document.querySelector('#cardContainer');
+
+    cardContainer.innerHTML = `
+    
+    `;
+};
+
+/*---------------FILTRAR FRUTA---------------*/
+iconSelectorFruits.addEventListener('click', event => {
+    //console.log(products[0].category);
+
+    const product = products.filter(filterFruit);
+
+    products.forEach( object => {
+        //console.log(object.category);
+        showProducts(object)
+
+        // if (object.category == 'frutas') {
+        //     console.log('fruits');
+
+
+        // } else {
+        // }
+    })
+});
+
+function filterFruit(products) {
+    if (products.category ) {
+        return products.category
+    }
+}
