7d9ead2ed05a2e0c6d82b230986a16c89351029a
Alduk3 <Alduk0110@gmail.com>
2024-01-30 09:15:18 -0500

    A- añadi el modal del menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 
 
 const btn = document.querySelector("#btn-initial");
-const header = document.querySelector("header")
+const header = document.querySelector("header");
+const btnMenu = document.querySelector('#btn-menu');
 
 btn.addEventListener("click",()=>{
     header.style.backgroundColor="#008000"
     header.style.display =" flex"
 
 })
+
+btnMenu.addEventListener('click', function () {
+    const modal = document.querySelector('.modal');
+    const btnExit = document.querySelector('#btn-exit');
+  
+    function openModal() {
+        modal.style.display = 'block';
+    }
+  
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+  
+
+    openModal();
+  
+
+    btnExit.addEventListener('click', function () {
+        closeModal();
+    });
+  
+
+  
+});
