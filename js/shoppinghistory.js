e9908d352cfc582f05b311f90af4a3d741d10336
manutorres93 <manutorres9312@gmail.com>
2024-01-31 08:26:58 -0500

    M- Organiznado el modal y su tabla en historial de compra

--- js/shoppinghistory.js
+++ js/shoppinghistory.js
@@ -31,14 +31,12 @@ const contenedorhistorialVacio = document.querySelector("#historial-vacio");
 const contenedorhistorialProductos = document.querySelector(
   "#historial-productos"
 );
-//const btnAbrirModal= document.querySelector('.btn-abrir-modal')
-const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
-//const modal= document.querySelector('#modal')
 
 function showHistory(userData) {
   console.log(userData.shopHistory);
 
   let arrayShopHistory = userData.shopHistory;
+  let arrayElementsBought = userData.shopHistory[0].shop;
 
   if (userData.shopHistory) {
     console.log(userData.name);
@@ -46,6 +44,7 @@ function showHistory(userData) {
     console.log(userData.shopHistory[0].price);
     console.log(userData.shopHistory[0].date);
     console.log(arrayShopHistory);
+    console.log(arrayElementsBought);
 
     contenedorhistorialVacio.classList.add("disabled");
     contenedorhistorialProductos.classList.remove("disabled");
@@ -84,183 +83,47 @@ function showHistory(userData) {
 
       contenedorhistorialProductos.append(div);
 
-
-    /* Funcionalidad para el modal */
-      const btnAbrirModal = document.querySelector(".btn-abrir-modal");
-      const btnCerrarModal = document.getElementById("btn-cerrar-modal");
-      const btnCerrarModalAux = document.getElementById("btn-cerrar-modal-aux");
-
-      btnAbrirModal.addEventListener("click", () => {
-        const modal = document.getElementById("modal");
-        modal.showModal(); 
-      });
-
-      
-      btnCerrarModal.addEventListener("click", () => {
-        modal.close();
-      });
-
-      btnCerrarModalAux.addEventListener("click", () => {
-        modal.close();
-      });
+      modalAcciones(arrayElementsBought);
     });
   } else {
     console.log("no hay compras");
   }
 }
 
-btnAbrirModal.addEventListener("click", () => {
-  modal.show();
-  console.log("hola!|");
-});
+function modalAcciones(arrayElementsBought) {
+  /* Funcionalidad para el modal */
+  const btnAbrirModal = document.querySelector(".btn-abrir-modal");
+  const btnCerrarModal = document.getElementById("btn-cerrar-modal");
+  const btnCerrarModalAux = document.getElementById("btn-cerrar-modal-aux");
 
-/* Traer información y pintarla en HTML */
+  btnAbrirModal.addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    modal.showModal();
+  });
 
-// /* ------CARGA INICIAL DE LOCALSTORAGE SETEANDO PRODUCTOS PARA EJEMPLO---- */
+  btnCerrarModal.addEventListener("click", () => {
+    modal.close();
+  });
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const products_cart = JSON.parse(localStorage.getItem("products_cart"));
+  btnCerrarModalAux.addEventListener("click", () => {
+    modal.close();
+  });
 
-//   if (products_cart) {
-//     cargarProductoshistorial();
-//   } else {
-//     //lo más seguro es que tenga que eliminar esto cuando ya funcione lo de juanes, ya que lo que quiero cargar es si o si lo que hay en local storage
+  //let arrayElementsBought= userData.shopHistory[0].shop
+  const tTable = document.querySelector(".table");
 
-//     localStorage.setItem("products_cart", JSON.stringify(products));
-//   }
+  arrayElementsBought.forEach((element) => {
+    const { name, quantity } = element;
 
-//   /* Para setear el localStorage del carrito*/
-//   /*  localStorage.removeItem('products_cart')  */
-// });
-
-/* ------SELECTORES--------- */
-
-// const contenedorhistorialVacio = document.querySelector("#historial-vacio");
-// const contenedorhistorialProductos = document.querySelector("#historial-productos");
-// const contenedorhistorialAcciones = document.querySelector("#historial-acciones");
-// const botonVaciar = document.querySelector("#historial-acciones-vaciar");
-// const contenedorTotal = document.querySelector("#total");
-// const botonComprar = document.querySelector("#historial-acciones-comprar");
-
-// /* -----TRAER PRODUCTOS DEL LOCALSTORAGE */
-// const products_cart = JSON.parse(localStorage.getItem("products_cart")); //productos en carito
-// console.log(products_cart);
-
-// /* Pintar el carrito de compras dentro del perfil */
-
-// function cargarProductoshistorial() {
-//   //Carga todo lo que hay en localStorage
-
-//   if (products_cart && products_cart.length > 0) {
-//     contenedorhistorialVacio.classList.add("disabled");
-//     contenedorhistorialProductos.classList.remove("disabled");
-//     contenedorhistorialAcciones.classList.remove("disabled");
-//     //contenedorhistorialComprado.classList.add("disabled");
-
-//     contenedorhistorialProductos.innerHTML = "";
-
-//     products_cart.forEach((product) => {
-//       const { image, name, pricePound, quantity, id } = product;
-
-//       const div = document.createElement("div");
-//       div.classList.add("historial-producto");
-//       div.innerHTML = `
-//                     <img class="historial-producto-imagen" src="/Img/${image}" alt="${image}">
-//                     <div class="historial-producto-titulo">
-//                         <small>Producto</small>
-//                         <h3>${name}</h3>
-//                     </div>
-//                     <div class="historial-producto-cantidad">
-//                         <small>Cantidad</small>
-//                         <p>${quantity}</p>
-//                     </div>
-//                     <div class="historial-producto-precio">
-//                         <small>Precio</small>
-//                         <p>${pricePound}</p>
-//                     </div>
-//                     <div class="historial-producto-subtotal">
-//                         <small>Subtotal</small>
-//                         <p>${quantity * pricePound}</p>
-//                     </div>
-//                     <button class="historial-producto-eliminar" id="${id}"><i class="bi bi-trash-fill"></i></button>
-//                 `;
-
-//       contenedorhistorialProductos.append(div);
-//     });
-//   } else {
-//     contenedorhistorialVacio.classList.remove("disabled");
-//     contenedorhistorialProductos.classList.add("disabled");
-//     contenedorhistorialAcciones.classList.add("disabled");
-//   }
-
-//   actualizarBotonesEliminar();
-
-//   actualizarTotal();
-// }
-
-// /* ---Función para eliminar productos del LS, por ende, del historial */
-
-// let botonesEliminar = document.querySelectorAll(".historial-producto-eliminar"); //es let porque se carga inicialmente otras cosas y tiene que volver a cargarse
-
-// function actualizarBotonesEliminar() {
-//   botonesEliminar = document.querySelectorAll(".historial-producto-eliminar"); //para que se reasignen cada vez que se creen los productos en el for each
-
-//   botonesEliminar.forEach((boton) => {
-//     boton.addEventListener("click", eliminarDelhistorial);
-//   });
-// }
-
-// function eliminarDelhistorial(e) {
-//   const idBoton = e.currentTarget.id;
-//   console.log(idBoton); //me imprime el id del producto al que estoy clickeando
-
-//   const idBotonString = idBoton.toString();
-
-//   //Buscar cual es el producto en el array
-
-//   const productoEliminado = products_cart.find(
-//     (product) => product.id.toString() === idBotonString
-//   );
-
-//   console.log(productoEliminado);
-
-//   const index = products_cart.findIndex(
-//     (product) => product.id.toString() === idBotonString
-//   );
-
-//   console.log(products_cart);
-//   products_cart.splice(index, 1);
-//   console.log(products_cart);
-
-//   cargarProductoshistorial(); //Hasta aqui se elimina y se pinta pero si recargo se vuelve a cargar todo porque no se ha eliminado de LS
-
-//   localStorage.setItem("products_cart", JSON.stringify(products_cart));
-// }
-
-// /* ---Función para vaciar el historial entero */
-
-// botonVaciar.addEventListener("click", vaciarhistorial);
-
-// function vaciarhistorial() {
-//   products_cart.length = 0;
-//   localStorage.setItem("products_cart", JSON.stringify(products_cart));
-//   cargarProductoshistorial();
-// }
-
-// /* --- Función para actualizar el total calculado */
-
-// function actualizarTotal() {
-//   const totalCalculado = products_cart.reduce(
-//     (acc, product) => acc + product.pricePound * product.quantity,
-//     0
-//   );
-
-//   total.innerText = `$${totalCalculado}`;
-// }
-
-// /* ---Función para redireccionar para pagina dde compras */
-
-// botonComprar.addEventListener("click", () => {
-//   // Redirigir a index.html por ahora
-//   window.location.href = "index.html";
-// });
+    console.log(name);
+    tTable.innerHTML += `
+   
+            <tr>
+            <td>${name}</td>
+            <td>${name}</td>
+            <td>${quantity}</td>
+            </tr>
+            
+  `;
+  });
+}
